789fde453ad9e91f86c3522e46f63e0522747c5c
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-08-25 14:13:01 +0100

    added left banner

--- src/containers/hero_banner/HeroBanner.tsx
+++ src/containers/hero_banner/HeroBanner.tsx
@@ -1,15 +1,16 @@
+import './HeroBanner.scss'
+
 const HeroBanner = () => {
   return (
     <div className='hero__banner container__grid'>
       <div className="left">
         <div className="top">
             <h1>Kyiv</h1>
-            <h1>LuxeBouquets</h1>
+            <div><h1>LuxeBouquets</h1></div>
             <p>Discover Uniquely Crafted Bouquets and Gifts for Any Occasion: Spread Joy with Our Online Flower Delivery Service</p>
         </div>
         <div className="bottom">
             <img src="/images/banner_hero.png" alt="" />
-            <div className="line"></div>
             <p>Experience the joy of giving with our modern floral studio. Order online and send flowers, plants and gifts today</p>
         </div>
       </div>
